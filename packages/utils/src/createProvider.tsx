267ffba7a91007851d98f851fbf29d926737ce48
Andrei Kochetygov <kochetygov.andrey.ge@gmail.com>
2023-12-20 01:05:53 +0400

    fixed hook return type in provider creation

--- packages/utils/src/createProvider.tsx
+++ packages/utils/src/createProvider.tsx
@@ -6,10 +6,13 @@ export function createProvider<
   // eslint-disable-next-line @typescript-eslint/no-explicit-any -- we don't know what the props will be passed
   ComputeProvidableValueCallback extends (props: any) => ProvidableValue,
 >(name: string, computeProvidableValue: ComputeProvidableValueCallback) {
-  const [Context, useContext] = createProviderContext<ProvidableValue>(name);
+  const [Context, useContext] = createProviderContext<ReturnType<ComputeProvidableValueCallback>>(name);
 
   const Provider = <Props extends Parameters<ComputeProvidableValueCallback>[0]>(props: Props) => {
-    const providableValue = useMemo(() => computeProvidableValue(props), [props]);
+    const providableValue = useMemo(
+      () => computeProvidableValue(props) as ReturnType<ComputeProvidableValueCallback>,
+      [props],
+    );
 
     return <Context.Provider value={providableValue}>{props.children}</Context.Provider>;
   };
